f8bf0d3ce25161884da492042e2beaf5d393f06a
kephalosk <watchfromtheshore@gmail.com>
2024-10-12 13:39:30 +0200

    fe-22 integrate prettier with eslint

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -7,6 +7,7 @@ import reactRefresh from 'eslint-plugin-react-refresh';
 export default [
   { ignores: ['dist'] },
   {
+    extends: ['react-app', 'plugin:prettier/recommended'],
     files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
@@ -28,6 +29,7 @@ export default [
       ...react.configs.recommended.rules,
       ...react.configs['jsx-runtime'].rules,
       ...reactHooks.configs.recommended.rules,
+      'prettier/prettier': ['error'],
       'react/jsx-no-target-blank': 'off',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
     },
