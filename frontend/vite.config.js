38c4931b591c8dc0e12c3ef83cb41edd4a589a6e
kephalosk <watchfromtheshore@gmail.com>
2024-10-12 15:51:44 +0200

    fe-22 add babel and refactor project-folder structure

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import babel from 'vite-plugin-babel';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    babel(),
+    // babel({
+    //   babelConfig: './.babelrc', // Optional: kann weggelassen werden, wenn die Babel-Konfiguration in der Standarddatei vorhanden ist
+    // }),
+  ],
 });
