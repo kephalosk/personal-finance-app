140fc5829bce19cb06e096f9850b34d46b70c294
kephalosk <watchfromtheshore@gmail.com>
2024-10-13 10:57:59 +0200

    fe-1 add static html structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,29 +1,38 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from './assets/vite.svg';
 import './App.scss';
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      <section className="sidebar">
+        <h1>Philipp Kraatz&#39; finance</h1>
+        <div className="sidebarList">
+          <div className="sidebarListEntry">
+            <img src="./src/assets/images/icon-nav-overview.svg" alt="overview icon" aria-hidden="true" />
+            <label>Overview</label>
+          </div>
+          <div className="sidebarListEntry">
+            <img src="./src/assets/images/icon-nav-transactions.svg" alt="transactions icon" aria-hidden="true" />
+            <label>Transactions</label>
+          </div>
+          <div className="sidebarListEntry">
+            <img src="./src/assets/images/icon-nav-budgets.svg" alt="budgets icon" aria-hidden="true" />
+            <label>Budgets</label>
+          </div>
+          <div className="sidebarListEntry">
+            <img src="./src/assets/images/icon-nav-pots.svg" alt="pots icon" aria-hidden="true" />
+            <label>Pots</label>
+          </div>
+          <div className="sidebarListEntry">
+            <img src="./src/assets/images/icon-nav-recurring-bills.svg" alt="recurring bills icon" aria-hidden="true" />
+            <label>Recurring Bills</label>
+          </div>
+        </div>
+        <div className="sidebarShrink">
+          <img src="./src/assets/images/icon-minimize-menu.svg" alt="minimize icon" aria-hidden="true" />
+          <label>Minimize Menu</label>
+        </div>
+      </section>
+      <section className="content"></section>
     </>
   );
 }
