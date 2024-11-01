a7dfe26707bb8b53676ee3f8caa4d09a000ab033
kephalosk <watchfromtheshore@gmail.com>
2024-11-01 10:43:52 +0100

    fe-17 add html structure

--- frontend/src/Pages/BillsPage.tsx
+++ frontend/src/Pages/BillsPage.tsx
@@ -5,6 +5,102 @@ export function BillsPage() {
     <>
       <div className="billsPage" data-testid="bills-page">
         <h1>Recurring Bills</h1>
+        <div className="billsPageDetails">
+          <div className="billsPageDetailsOverview">
+            <div className="billTotal">
+              <img className="billTotalImage" />
+              <label className="billTotalTitle"></label>
+              <label className="billTotalAmount"></label>
+            </div>
+            <div className="billSummary">
+              <div className="billSummaryTitle">
+                <div className="billSummaryRow"></div>
+                <hr />
+                <div className="billSummaryRow"></div>
+                <hr />
+                <div className="billSummaryRow"></div>
+              </div>
+            </div>
+          </div>
+          <div className="billCard">
+            <div className="billCardSearchbar"></div>
+            <div className="billCardTable">
+              <div className="billCardTableHeader"></div>
+              <hr />
+              <div className="billCardTableRow">
+                <div className="billCardTableRowName">
+                  <img className="billCardTableRowNamePicture" />
+                  <label className="billCardTableRowNameLabel"></label>
+                </div>
+                <label className="billCardTableRowDate"></label>
+                <label className="billCardTableRowAmount"></label>
+              </div>
+              <hr />
+              <div className="billCardTableRow">
+                <div className="billCardTableRowName">
+                  <img className="billCardTableRowNamePicture" />
+                  <label className="billCardTableRowNameLabel"></label>
+                </div>
+                <label className="billCardTableRowDate"></label>
+                <label className="billCardTableRowAmount"></label>
+              </div>
+              <hr />
+              <div className="billCardTableRow">
+                <div className="billCardTableRowName">
+                  <img className="billCardTableRowNamePicture" />
+                  <label className="billCardTableRowNameLabel"></label>
+                </div>
+                <label className="billCardTableRowDate"></label>
+                <label className="billCardTableRowAmount"></label>
+              </div>
+              <hr />
+              <div className="billCardTableRow">
+                <div className="billCardTableRowName">
+                  <img className="billCardTableRowNamePicture" />
+                  <label className="billCardTableRowNameLabel"></label>
+                </div>
+                <label className="billCardTableRowDate"></label>
+                <label className="billCardTableRowAmount"></label>
+              </div>
+              <hr />
+              <div className="billCardTableRow">
+                <div className="billCardTableRowName">
+                  <img className="billCardTableRowNamePicture" />
+                  <label className="billCardTableRowNameLabel"></label>
+                </div>
+                <label className="billCardTableRowDate"></label>
+                <label className="billCardTableRowAmount"></label>
+              </div>
+              <hr />
+              <div className="billCardTableRow">
+                <div className="billCardTableRowName">
+                  <img className="billCardTableRowNamePicture" />
+                  <label className="billCardTableRowNameLabel"></label>
+                </div>
+                <label className="billCardTableRowDate"></label>
+                <label className="billCardTableRowAmount"></label>
+              </div>
+              <hr />
+              <div className="billCardTableRow">
+                <div className="billCardTableRowName">
+                  <img className="billCardTableRowNamePicture" />
+                  <label className="billCardTableRowNameLabel"></label>
+                </div>
+                <label className="billCardTableRowDate"></label>
+                <label className="billCardTableRowAmount"></label>
+              </div>
+              <hr />
+              <div className="billCardTableRow">
+                <div className="billCardTableRowName">
+                  <img className="billCardTableRowNamePicture" />
+                  <label className="billCardTableRowNameLabel"></label>
+                </div>
+                <label className="billCardTableRowDate"></label>
+                <label className="billCardTableRowAmount"></label>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
