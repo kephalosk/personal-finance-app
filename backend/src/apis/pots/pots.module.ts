30ddf4b8bf12b983d12dd5c2ea30a4ab8fcccdf9
kephalosk <watchfromtheshore@gmail.com>
2024-11-17 22:06:46 +0100

    be-4 add TypeORM dependencies

--- backend/src/apis/pots/pots.module.ts
+++ backend/src/apis/pots/pots.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PotsController } from './pots.controller';
 import { PotsService } from './pots.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Pot } from '../../model/entities/pot.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Pot])],
   controllers: [PotsController],
   providers: [PotsService],
 })
