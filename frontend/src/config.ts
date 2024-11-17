5f5cf99e8d959012513c3d49bfcaf7c4169678a2
kephalosk <watchfromtheshore@gmail.com>
2024-11-17 12:52:05 +0100

    df-6 fix backend path

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,7 +2,7 @@ export interface Config {
   API_BACKEND_HOST: string;
 }
 
-const API_BACKEND_HOST = 'https://backend.philippkraatz.com';
+const API_BACKEND_HOST = 'https://backend.philippkraatz.com/api';
 
 export const AppConfig: Config = {
   API_BACKEND_HOST,
