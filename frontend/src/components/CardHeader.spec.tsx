1f672389ca20f3079c82501b9036387220a109a8
kephalosk <watchfromtheshore@gmail.com>
2024-12-22 20:55:35 +0100

    fe-24 integrate CardHeaderDropdownList in CardHeader

--- frontend/src/components/CardHeader.spec.tsx
+++ frontend/src/components/CardHeader.spec.tsx
@@ -1,19 +1,47 @@
-import { render } from '@testing-library/react';
-import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import React, { ReactNode } from 'react';
 import CardHeader from './CardHeader';
+import { CardHeaderItemNameEnum } from '../model/enum/CardHeaderItemNameEnum';
+import CardHeaderDropdownList from './atoms/CardHeaderDropdownList';
+import { CardHeaderItemOperationEnum } from '../model/enum/CardHeaderItemOperationEnum';
+
+const itemOperation = CardHeaderItemNameEnum.BUDGET;
+const itemName = CardHeaderItemNameEnum.BUDGET;
+jest.mock(
+  './atoms/CardHeaderDropdownList',
+  (): jest.Mock =>
+    jest.fn(
+      (props): ReactNode => (
+        <div
+          data-testid="card-header-dropdown-list"
+          onClick={props.handleSelection(itemOperation, itemName)}
+        ></div>
+      )
+    )
+);
 
 describe('CardHeader', () => {
   const title = 'testTitle';
   const color = 'testColor';
+  const mockHandleSelection = jest.fn();
 
-  const testProps = {
+  const testProps: {
+    title: string;
+    color: string;
+    itemName: CardHeaderItemNameEnum;
+    handleSelection: (
+      itemOperation: CardHeaderItemOperationEnum,
+      itemName: CardHeaderItemNameEnum
+    ) => void;
+  } = {
     title,
     color,
+    itemName,
+    handleSelection: mockHandleSelection,
   };
 
   it('renders div CardHeader', () => {
     const { container } = render(<CardHeader {...testProps} />);
-
     const htmlElement = container.querySelector('.cardHeader');
 
     expect(htmlElement).toBeInTheDocument();
@@ -47,5 +75,31 @@ describe('CardHeader', () => {
     expect(htmlElement).toHaveAttribute('aria-hidden', 'true');
     expect(htmlElement).toHaveAttribute('src', '/images/icon-ellipsis.svg');
     expect(htmlElement).toHaveAttribute('tabIndex', '0');
+  });
+
+  it('renders component CardHeaderDropdownList', () => {
+    render(<CardHeader {...testProps} />);
+
+    const component = screen.getByTestId('card-header-dropdown-list');
+
+    expect(component).toBeInTheDocument();
+    expect(CardHeaderDropdownList).toHaveBeenCalledWith(
+      {
+        handleSelection: mockHandleSelection,
+        hideDropdown: expect.any(Function),
+        isDropdownVisible: false,
+        itemName,
+      },
+      {}
+    );
+  });
+
+  it('calls handleSelection when component CardHeaderDropdownList is clicked', () => {
+    render(<CardHeader {...testProps} />);
+
+    const component = screen.getByTestId('card-header-dropdown-list');
+    fireEvent.click(component);
+
+    expect(mockHandleSelection).toHaveBeenCalledWith(itemOperation, itemName);
   });
 });
--- frontend/src/components/CardHeader.tsx
+++ frontend/src/components/CardHeader.tsx
@@ -1,15 +1,41 @@
 import './CardHeader.scss';
-import React from 'react';
+import React, { KeyboardEvent, useState } from 'react';
+import CardHeaderDropdownList from './atoms/CardHeaderDropdownList';
+import { CardHeaderItemNameEnum } from '../model/enum/CardHeaderItemNameEnum';
+import { CardHeaderItemOperationEnum } from '../model/enum/CardHeaderItemOperationEnum';
 
 interface Props {
   title: string;
   color: string;
+  itemName: CardHeaderItemNameEnum;
+  handleSelection: (
+    itemOperation: CardHeaderItemOperationEnum,
+    itemName: CardHeaderItemNameEnum
+  ) => void;
 }
 
-const CardHeader: ({ title, color }: Props) => React.ReactNode = ({
+const CardHeader: ({ title, color, itemName }: Props) => React.ReactNode = ({
   title,
   color,
+  itemName,
+  handleSelection,
 }: Props): React.ReactNode => {
+  const [isDropdownVisible, setIsDropdownVisible] = useState<boolean>(false);
+
+  const handleClick = () => {
+    setIsDropdownVisible(true);
+  };
+
+  const hideDropdown = () => {
+    setIsDropdownVisible(false);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    if (event.key === 'Enter') {
+      setIsDropdownVisible(true);
+    }
+  };
+
   return (
     <>
       <div className="cardHeader" data-testid="card-header">
@@ -22,11 +48,15 @@ const CardHeader: ({ title, color }: Props) => React.ReactNode = ({
             aria-hidden="true"
             src="/images/icon-ellipsis.svg"
             tabIndex={0}
+            onClick={handleClick}
+            onKeyDown={(event: KeyboardEvent) => handleKeyDown(event)}
           />
-          <div className="cardHeaderEditDropdown">
-            <div className="cardHeaderEditDropdownEdit dropdownItem"></div>
-            <div className="cardHeaderEditDropdownDelete dropdownItem"></div>
-          </div>
+          <CardHeaderDropdownList
+            itemName={itemName}
+            isDropdownVisible={isDropdownVisible}
+            handleSelection={handleSelection}
+            hideDropdown={hideDropdown}
+          />
         </div>
       </div>
     </>
