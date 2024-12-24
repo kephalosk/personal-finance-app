dae8f102c15a3d715839c0502484658472a54129
kephalosk <watchfromtheshore@gmail.com>
2024-12-24 04:28:31 +0100

    fe-24 fix unit tests of BudgetCard

--- frontend/src/components/atoms/InputMoney.tsx
+++ frontend/src/components/atoms/InputMoney.tsx
@@ -6,6 +6,7 @@ import {
   ForwardRefExoticComponent,
   ReactNode,
   RefAttributes,
+  useEffect,
   useImperativeHandle,
   useState,
 } from 'react';
@@ -13,6 +14,7 @@ import {
 interface Props {
   handleInputChange: (input: number) => void;
   hasValidInput: boolean;
+  initialValue?: string;
 }
 
 export interface InputMoneyRef {
@@ -22,70 +24,82 @@ export interface InputMoneyRef {
 const InputMoney: ForwardRefExoticComponent<Props & RefAttributes<InputMoneyRef>> = forwardRef<
   InputMoneyRef,
   Props
->(({ handleInputChange, hasValidInput }: Props, ref: ForwardedRef<InputMoneyRef>): ReactNode => {
-  const [value, setValue] = useState<string>('');
+>(
+  (
+    { handleInputChange, hasValidInput, initialValue = '' }: Props,
+    ref: ForwardedRef<InputMoneyRef>
+  ): ReactNode => {
+    const [value, setValue] = useState<string>(initialValue);
 
-  useImperativeHandle(ref, (): { reset: () => void } => ({
-    reset: (): void => {
+    useEffect(() => {
+      setValue(initialValue);
+    }, [initialValue]);
+
+    useImperativeHandle(ref, (): { reset: () => void } => ({
+      reset: (): void => {
+        setValue(initialValue);
+        if (initialValue) {
+          handleInputChange(parseInt(initialValue, 10));
+        }
+      },
+    }));
+
+    const handleChange: (event: ChangeEvent<HTMLInputElement>) => void = (
+      event: ChangeEvent<HTMLInputElement>
+    ): void => {
+      const rawValue: string = removeInvalidCharacters(event);
+      const numericValue: number = checkUpperLimit(rawValue);
+      setNewInput(numericValue);
+    };
+
+    const removeInvalidCharacters: (event: ChangeEvent<HTMLInputElement>) => string = (
+      event: ChangeEvent<HTMLInputElement>
+    ): string => {
+      return event.target.value.replace(/[^0-9]/g, '');
+    };
+
+    const checkUpperLimit: (numericValue: string) => number = (rawValue: string): number => {
+      const numericValue: number = parseInt(rawValue, 10);
+      if (numericValue > 1000000000000) {
+        return 1000000000000;
+      }
+      return numericValue;
+    };
+
+    const setNewInput: (numericValue: number) => void = (numericValue: number): void => {
+      if (numericValue) {
+        const formattedValue: string = numericValue.toLocaleString('en-US');
+        setValue(formattedValue);
+        handleInputChange(numericValue);
+      } else {
+        resetInput();
+      }
+    };
+
+    const resetInput: () => void = (): void => {
       setValue('');
-    },
-  }));
+      handleInputChange(0);
+    };
 
-  const handleChange: (event: ChangeEvent<HTMLInputElement>) => void = (
-    event: ChangeEvent<HTMLInputElement>
-  ): void => {
-    const rawValue: string = removeInvalidCharacters(event);
-    const numericValue: number = checkUpperLimit(rawValue);
-    setNewInput(numericValue);
-  };
-
-  const removeInvalidCharacters: (event: ChangeEvent<HTMLInputElement>) => string = (
-    event: ChangeEvent<HTMLInputElement>
-  ): string => {
-    return event.target.value.replace(/[^0-9]/g, '');
-  };
-
-  const checkUpperLimit: (numericValue: string) => number = (rawValue: string): number => {
-    const numericValue: number = parseInt(rawValue, 10);
-    if (numericValue > 1000000000000) {
-      return 1000000000000;
-    }
-    return numericValue;
-  };
-
-  const setNewInput: (numericValue: number) => void = (numericValue: number): void => {
-    if (numericValue) {
-      const formattedValue: string = numericValue.toLocaleString('en-US');
-      setValue(formattedValue);
-      handleInputChange(numericValue);
-    } else {
-      resetInput();
-    }
-  };
-
-  const resetInput: () => void = (): void => {
-    setValue('');
-    handleInputChange(0);
-  };
-
-  return (
-    <div className="inputMoneyContainer">
-      <div className="inputMoney" data-testid="input-money">
-        <input
-          className="inputMoneyInput"
-          placeholder="e.g. 2000"
-          value={value}
-          onChange={handleChange}
-          type="text"
-        />
-        <label className={`inputMoneyIcon ${value ? 'active' : ''}`}>$</label>
+    return (
+      <div className="inputMoneyContainer">
+        <div className="inputMoney" data-testid="input-money">
+          <input
+            className="inputMoneyInput"
+            placeholder="e.g. 2000"
+            value={value}
+            onChange={handleChange}
+            type="text"
+          />
+          <label className={`inputMoneyIcon ${value ? 'active' : ''}`}>$</label>
+        </div>
+        <label className={`inputMoneyValidation ${hasValidInput ? '' : 'visible'}`}>
+          Please enter a spending limit!
+        </label>
       </div>
-      <label className={`inputMoneyValidation ${hasValidInput ? '' : 'visible'}`}>
-        Please enter a spending limit!
-      </label>
-    </div>
-  );
-});
+    );
+  }
+);
 
 InputMoney.displayName = 'InputMoney';
 
--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -2,8 +2,8 @@ interface Config {
   API_BACKEND_HOST: string;
 }
 
-const API_BACKEND_HOST = 'https://backend.philippkraatz.com/api';
-// const API_BACKEND_HOST = 'http://localhost:3000';
+// const API_BACKEND_HOST = 'https://backend.philippkraatz.com/api';
+const API_BACKEND_HOST = 'http://localhost:3000';
 
 export const AppConfig: Config = {
   API_BACKEND_HOST,
