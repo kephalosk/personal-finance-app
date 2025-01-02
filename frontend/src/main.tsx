3b031910b262c4775f52f8509e9de02ce33a11cb
kephalosk <watchfromtheshore@gmail.com>
2025-01-02 15:18:39 +0100

    fe-26 wrap SidebarProvider around App

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,19 +1,25 @@
-import { StrictMode } from 'react';
+import { ReactElement, StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.scss';
+import SidebarProvider from './components/provider/SidebarProvider';
 
-export const renderApp = () => {
+export const renderApp = (): ReactElement | undefined => {
   const rootElement: HTMLElement | null = document.getElementById('root');
 
   if (rootElement) {
-    createRoot(rootElement).render(
+    const content: ReactElement = (
       <StrictMode>
-        <App />
+        <SidebarProvider>
+          <App />
+        </SidebarProvider>
       </StrictMode>
     );
+    createRoot(rootElement).render(content);
+    return content;
   } else {
     console.error('Root element not found');
+    return undefined;
   }
 };
 
