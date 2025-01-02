ec67dcf780e5f8d2874147e241e5730d3d7b389a
kephalosk <watchfromtheshore@gmail.com>
2025-01-02 18:26:39 +0100

    fe-26 create InputCustonName component

---
+++ frontend/src/showcase/ShowcaseAddNewPotForm.tsx
@@ -0,0 +1,28 @@
+import OverlayCardBox from '../components/overlay/OverlayCardBox';
+import ShowcaseWrapper from './ShowcaseWrapper';
+import OverlayContentAddNewPot from '../components/overlay/OverlayContentAddNewPot';
+
+const ShowcaseAddNewBudgetForm = () => {
+  const addNewPotDescription: string =
+    'Create a pot to set savings targets. These can help keep you on track as you save for special purchases.';
+  const isHidden: boolean = false;
+  const handleAddNewPot = () => {};
+  const closeForm = () => {};
+
+  return (
+    <ShowcaseWrapper>
+      <OverlayCardBox
+        title="Add New Pot"
+        description={addNewPotDescription}
+        submitText="Add Pot"
+        isHidden={isHidden}
+        handleEvent={handleAddNewPot}
+        onClose={closeForm}
+      >
+        <OverlayContentAddNewPot />
+      </OverlayCardBox>
+    </ShowcaseWrapper>
+  );
+};
+
+export default ShowcaseAddNewBudgetForm;
