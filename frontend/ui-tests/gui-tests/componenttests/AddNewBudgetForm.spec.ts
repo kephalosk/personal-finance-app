ab8453a7fcbbcf71fc59ae91f2657acbcb4c9b70
kephalosk <watchfromtheshore@gmail.com>
2025-01-06 13:24:03 +0100

    fe-26 add gut tests

--- frontend/ui-tests/gui-tests/componenttests/AddNewBudgetForm.spec.ts
+++ frontend/ui-tests/gui-tests/componenttests/AddNewBudgetForm.spec.ts
@@ -1,16 +1,14 @@
 import { test, expect } from '@playwright/test';
 import openPage from '../utils/openPage';
 import { DeviceHeight, DeviceWidth } from '../constants';
+import removeFocus from '../utils/removeFocus';
 
 const BASE_PATH: string = 'http://localhost:5173/showcase/AddNewBudgetForm';
 
 test('AddNewBudgetForm-Desktop', async ({ page }): Promise<void> => {
   await openPage(page, BASE_PATH, DeviceHeight.Default, DeviceWidth.Desktop);
 
-  await page.evaluate(() => {
-    const activeElement = document.activeElement as HTMLElement;
-    if (activeElement) activeElement.blur();
-  });
+  await removeFocus(page);
 
   await expect(page).toHaveScreenshot();
 });
@@ -18,10 +16,7 @@ test('AddNewBudgetForm-Desktop', async ({ page }): Promise<void> => {
 test('AddNewBudgetForm-Tablet', async ({ page }): Promise<void> => {
   await openPage(page, BASE_PATH, DeviceHeight.Default, DeviceWidth.Tablet);
 
-  await page.evaluate(() => {
-    const activeElement = document.activeElement as HTMLElement;
-    if (activeElement) activeElement.blur();
-  });
+  await removeFocus(page);
 
   await expect(page).toHaveScreenshot();
 });
@@ -29,10 +24,7 @@ test('AddNewBudgetForm-Tablet', async ({ page }): Promise<void> => {
 test('AddNewBudgetForm-Mobile', async ({ page }): Promise<void> => {
   await openPage(page, BASE_PATH, DeviceHeight.Default, DeviceWidth.Mobile);
 
-  await page.evaluate(() => {
-    const activeElement = document.activeElement as HTMLElement;
-    if (activeElement) activeElement.blur();
-  });
+  await removeFocus(page);
 
   await expect(page).toHaveScreenshot();
 });
