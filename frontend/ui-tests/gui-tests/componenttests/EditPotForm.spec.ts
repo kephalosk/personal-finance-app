ec68776d1d49332018e8ab76ef8f6ef0489f15a7
kephalosk <watchfromtheshore@gmail.com>
2025-01-07 13:38:08 +0100

    add gui tests

---
+++ frontend/ui-tests/gui-tests/componenttests/EditPotForm.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+import openPage from '../utils/openPage';
+import { DeviceHeight, DeviceWidth } from '../constants';
+import removeFocus from '../utils/removeFocus';
+
+const BASE_PATH: string = 'http://localhost:5173/showcase/EditPotForm';
+
+test('EditPotForm-Desktop', async ({ page }): Promise<void> => {
+  await openPage(page, BASE_PATH, DeviceHeight.Default, DeviceWidth.Desktop);
+
+  await removeFocus(page);
+
+  await expect(page).toHaveScreenshot();
+});
+
+test('EditPotForm-Tablet', async ({ page }): Promise<void> => {
+  await openPage(page, BASE_PATH, DeviceHeight.Default, DeviceWidth.Tablet);
+
+  await removeFocus(page);
+
+  await expect(page).toHaveScreenshot();
+});
+
+test('EditPotForm-Mobile', async ({ page }): Promise<void> => {
+  await openPage(page, BASE_PATH, DeviceHeight.Default, DeviceWidth.Mobile);
+
+  await removeFocus(page);
+
+  await expect(page).toHaveScreenshot();
+});
