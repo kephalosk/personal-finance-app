0e5cadd2dee6c71ad6be07bd34a5b0292d480d3e
kephalosk <watchfromtheshore@gmail.com>
2025-01-10 18:46:16 +0100

    fe-29 add gui-tests

---
+++ frontend/ui-tests/gui-tests/componenttests/PotProgressForm.spec.ts
@@ -0,0 +1,30 @@
+import { test, expect } from '@playwright/test';
+import openPage from '../utils/openPage';
+import { DeviceHeight, DeviceWidth } from '../constants';
+import removeFocus from '../utils/removeFocus';
+
+const BASE_PATH: string = 'http://localhost:5173/showcase/PotProgressForm';
+
+test('PotProgressForm-Desktop', async ({ page }): Promise<void> => {
+  await openPage(page, BASE_PATH, DeviceHeight.Default, DeviceWidth.Desktop);
+
+  await removeFocus(page);
+
+  await expect(page).toHaveScreenshot();
+});
+
+test('PotProgressForm-Tablet', async ({ page }): Promise<void> => {
+  await openPage(page, BASE_PATH, DeviceHeight.Default, DeviceWidth.Tablet);
+
+  await removeFocus(page);
+
+  await expect(page).toHaveScreenshot();
+});
+
+test('PotProgressForm-Mobile', async ({ page }): Promise<void> => {
+  await openPage(page, BASE_PATH, DeviceHeight.Default, DeviceWidth.Mobile);
+
+  await removeFocus(page);
+
+  await expect(page).toHaveScreenshot();
+});
