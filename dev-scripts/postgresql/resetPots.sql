f805124e1b865fb55ed47dac11962bbe68901fd7
kephalosk <watchfromtheshore@gmail.com>
2025-01-10 19:25:02 +0100

    fe-30 udpate database script

--- dev-scripts/postgresql/resetPots.sql
+++ dev-scripts/postgresql/resetPots.sql
@@ -1,16 +1,17 @@
 DROP TABLE IF EXISTS pots;
 
 CREATE TABLE pots (
-                         id SERIAL PRIMARY KEY,
-                         name VARCHAR(255) NOT NULL,
-                         target NUMERIC(10, 2) NOT NULL,
-                         total NUMERIC(10, 2) NOT NULL,
-                         theme VARCHAR(7) NOT NULL
+                      id SERIAL PRIMARY KEY,
+                      name VARCHAR(255) NOT NULL,
+                      target NUMERIC(10, 2) NOT NULL,
+                      total NUMERIC(10, 2) NOT NULL,
+                      theme VARCHAR(7) NOT NULL
 );
 
-INSERT INTO budgets (name, target, total, theme)
+INSERT INTO pots (name, target, total, theme)
 VALUES
     ('Savings', 2000.00, 159.00, '#277C78'),
     ('Concert Ticket', 150.00, 110.00, '#82C9D7'),
     ('Gift', 150.00, 110.00, '#F2CDAC'),
-    ('New Laptop', 1000.00, 10.00, '#626070');
+    ('New Laptop', 1000.00, 10.00, '#626070'),
+    ('Holiday', 1440.00, 531.00, '#826CB0');
